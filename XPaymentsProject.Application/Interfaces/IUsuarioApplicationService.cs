71e2500174df3c07491be4882a9ad33e862b809c
ivanlmpk <ivanlempek@hotmail.com>
2023-11-24 15:54:30 -0300

    Adicionado o serviço de usuário.

---
+++ XPaymentsProject.Application/Interfaces/IUsuarioApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XPaymentsProject.Domain.Entities;
+
+namespace XPaymentsProject.Application.Interfaces
+{
+    public interface IUsuarioApplicationService
+    {
+        Task AddUsuario(string login, string senha, string nome, string email);
+        Task UpdateUsuario(int id, Usuario usuario);
+        Task DeleteUsuario(int id);
+        Task<Usuario> GetUsuarioById(int id);
+        Task GetAllUsuarios();
+    }
+}
--- XPaymentsProject.Application/Services/UsuarioApplicationService.cs
+++ XPaymentsProject.Application/Services/UsuarioApplicationService.cs
@@ -3,11 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XPaymentsProject.Application.Interfaces;
+using XPaymentsProject.Domain.Entities;
+using XPaymentsProject.Domain.Interfaces;
 
 namespace XPaymentsProject.Application.Services
 {
-    public class UsuarioApplicationService
+    public class UsuarioApplicationService : IUsuarioApplicationService
     {
+        private IGenericRepository<Usuario> _genericRepository;
 
+        public UsuarioApplicationService(IGenericRepository<Usuario> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public async Task GetAllUsuarios()
+        {
+            await _genericRepository.GetAllAsync();
+        }
+
+        public async Task<Usuario> GetUsuarioById(int id)
+        {
+            return await _genericRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddUsuario(string login, string senha, string nome, string email)
+        {
+            var usuario = new Usuario(login, senha, nome, email);
+
+            await _genericRepository.AddAsync(usuario);
+        }
+
+        public async Task UpdateUsuario(int id, Usuario usuario)
+        {
+            var getUsuario = await _genericRepository.GetByIdAsync(id);
+
+            getUsuario.Login = usuario.Login;
+            getUsuario.Senha = usuario.Senha;
+            getUsuario.Nome = usuario.Nome;
+            getUsuario.Email = usuario.Email;
+
+            await _genericRepository.UpdateAsync(getUsuario);
+        }
+
+        public async Task DeleteUsuario(int id)
+        {
+            await _genericRepository.DeleteAsync(id);
+        }
     }
 }
